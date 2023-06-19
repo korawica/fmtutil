d81b7fd42f94e51681faae36819d291ec30ea801
Korawich Anuttra <korawich.anu@gmail.com>
2023-06-19 15:09:52 +0700

    :dart: feat: edit pre-commit-msg hook

--- scripts/commit-msg-2.py
+++ scripts/commit-msg-2.py
@@ -1,6 +1,25 @@
-"""The commit-msg Git hook to check the commit message."""
+"""
+The commit-msg Git hook to check the commit message.
+docs: https://cbea.ms/git-commit/
+
+- Rule 01: Separate subject from body with a blank line
+- Rule 02: Limit the subject line to 50 characters
+- Rule 03: Capitalize the subject line
+- Rule 04: Do not end the subject line with a period
+- Rule 05: Use the imperative mood in the subject line
+- Rule 06: Wrap the body at 72 characters
+- Rule 07: Use the body to explain what and why vs. how
+"""
 import sys
 from enum import Enum
+from typing import Dict, List
+
+
+class Emoji(str, Enum):
+    FIX = ":gear:"
+    FEAT = ":dart:"
+    DOCS = ":page_facing_up:"
+    BUILD = ":toolbox:"
 
 
 class Bcolors(str, Enum):
@@ -26,15 +45,71 @@ class Level(str, Enum):
     ERROR = "ERROR"
 
 
-def print_with_color(message: str, level: Level) -> None:
+WORD_EMOJI: Dict[str, Emoji] = {
+    "feat": Emoji.FEAT,
+    "fix": Emoji.FIX,
+    "docs": Emoji.DOCS,
+    "build": Emoji.BUILD,
+    "workflow": Emoji.BUILD,
+}
+
+
+def print_color(message: str, level: Level) -> None:
     """Print the message with a color for the corresponding level."""
     print(
-        Bcolors[level]
-        + Bcolors.BOLD
-        + f"{level}: [Policy] "
-        + message
-        + Bcolors.ENDC
+        f"{Bcolors[level]}{Bcolors.BOLD}"
+        f"{level}: [Policy] {message}{Bcolors.ENDC}",
+        file=sys.stderr,
     )
+
+
+def prepare_subject(subject: str) -> str:
+    sub_lists: List[str] = [
+        x.strip().strip(".") for x in subject.split(":", maxsplit=1)
+    ]
+    if len(sub_lists) == 1:
+        sub_lists = ["feat", sub_lists[0]]
+
+    if (stype := sub_lists[0]) not in WORD_EMOJI:
+        print_color(
+            f"There should be set emoji for prefix {stype} of subject",
+            Level.ERROR,
+        )
+        sys.exit(1)
+
+    return f"{WORD_EMOJI[stype]} {stype}: {sub_lists[1]}"
+
+
+def validate_for_warning(lines: List[str]) -> bool:
+    has_warning: bool = False
+    subject: str = lines[0]
+    # RULE 02: Limit the subject line to 50 characters
+    if len(subject) <= 20 or len(subject) > 50:
+        has_warning = True
+        print_color(
+            "There should be between 21 and 50 characters in the commit title.",
+            Level.WARNING,
+        )
+
+    if len(lines) > 1:
+        if len(lines) <= 2:
+            has_warning = True
+            print_color(
+                "There should at least 3 lines in your commit message.",
+                Level.WARNING,
+            )
+
+        # RULE 01: Separate subject from body with a blank line
+        if lines[1].strip() != "":
+            has_warning = True
+            print_color(
+                "There should be an empty line between the commit title and body.",
+                Level.WARNING,
+            )
+
+    lines[0] = prepare_subject(subject)
+
+    return has_warning
 
 
 def check_commit_msg_pattern():
@@ -49,48 +124,45 @@ def check_commit_msg_pattern():
         lines = f_msg.readlines()
 
     # Remove the comment lines in the commit message.
-    lines = [line for line in lines if not line.strip().startswith("#")]
-
-    has_warning = False
-    if len(lines) < 4:
-        message = "There should at least 4 lines in your commit message."
-        print_with_color(message, Level.ERROR)
-        sys.exit(1)
-
-    if len(lines[0]) > 50:
-        has_warning = True
-        message = "There should be less then 50 characters in the commit title."
-        print_with_color(message, Level.WARNING)
-        sys.exit(1)
-
-    if lines[1].strip() != "":
-        has_warning = True
-        message = (
-            "There should be an empty line between the commit title and body."
+    lines: List[str] = [
+        line for line in lines if not line.strip().startswith("#")
+    ]
+    if not lines:
+        print_color(
+            "Please supply commit message without start with ``#``.",
+            Level.ERROR,
         )
-        print_with_color(message, Level.WARNING)
         sys.exit(1)
+
+    # Validate print warning message
+    has_warning: bool = validate_for_warning(lines)
 
     has_story_id = False
     for line in lines[2:]:
+        # RULE 06: Wrap the body at 72 characters
         if len(line) > 72:
-            has_warning = True
             message = "The commit body should wrap at 72 characters."
-            print_with_color(message, Level.WARNING)
+            print_color(message, Level.WARNING)
             sys.exit(1)
 
-        if "[#" in line:
+        if line.startswith("["):
             has_story_id = True
 
     if not has_story_id:
         message = "Please add a Story ID in the commit message."
-        print_with_color(message, Level.WARNING)
-        sys.exit(1)
+        print_color(message, Level.WARNING)
+        has_warning = True
 
     if not has_warning:
         message = "The commit message has the required pattern."
-        print_with_color(message, Level.OK)
+        print_color(message, Level.OK)
+
+    with open(msg_temp_file, mode="w", encoding="utf-8") as file:
+        file.write("\n".join(lines))
+
+    sys.exit(0)
 
 
 if __name__ == "__main__":
+    # TODO: change to `raise SystemExit(check_commit_msg_pattern())`
     check_commit_msg_pattern()
