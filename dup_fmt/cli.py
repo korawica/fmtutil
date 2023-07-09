90a3581a5cb154de28b59a52f018a927691fe6ae
Korawich Anuttra <korawich.anu@gmail.com>
2023-07-10 00:10:44 +0700

    feat: add cli for formatter package

--- dup_fmt/cli.py
+++ dup_fmt/cli.py
@@ -1,1 +1,24 @@
 from __future__ import annotations  # no cov
+
+import sys
+
+import click
+
+
+@click.group()
+def cli():
+    """A simple command line tool."""
+    pass  # pragma: no cover.
+
+
+@cli.command()
+def log():
+    sys.exit(0)
+
+
+def main() -> None:
+    cli.main()
+
+
+if __name__ == "__main__":
+    main()
