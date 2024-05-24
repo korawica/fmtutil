314856a24e477aff530b26cfe927962bef204ff0
Korawich Anuttra <korawich.anu@gmail.com>
2024-05-25 00:02:00 +0700

    :test_tube: tests: add test cases for escape regex work.

--- tests/test_escape.py
+++ tests/test_escape.py
@@ -1,9 +1,10 @@
+import re
 import unittest
 
 import fmtutil.utils as utils
 
 
-class UtilsTestCase(unittest.TestCase):
+class UtilsFormatEscapeTestCase(unittest.TestCase):
 
     def test_fmt_group_escape_utils(self):
         rs = utils.escape_fmt_group(
@@ -22,3 +23,17 @@ class UtilsTestCase(unittest.TestCase):
             r"\+file_{datetime:%Y-%m-%d %H:%M:%S}_{naming:%n_%e}\\\.json",
             rs,
         )
+
+    def test_fmt_escape_utils_simple(self):
+        rs = re.search(utils.escape_fmt_group(r"\w+"), "Test escape")
+        self.assertIsNone(rs)
+        rs = re.search(utils.escape_fmt_group(r"\w+"), r"Test escape\w+")
+        self.assertEqual(r"\w+", rs.group(0))
+
+    def test_fmt_escape_utils_replace(self):
+        rs = re.sub(
+            utils.escape_fmt_group("$date.data.json"),
+            "replace",
+            "Test escape: $date.data.json",
+        )
+        self.assertEqual("Test escape: replace", rs)
